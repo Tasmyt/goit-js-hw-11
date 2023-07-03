bb0f10ade54c00b5655ca166385e055838572650
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-07-03 08:53:11 +0300

    3 07

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,15 +1,13 @@
 const BASE_URL = `https://pixabay.com/api`;
 const key = '37986162-de52f9a52753fd2efa27d9e9b';
 
-
-
 async function fetchImages(searchQuery, page) {
-    const resp = await fetch(`${BASE_URL}/?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
-    return await resp.json();
-        };
-
+  const resp = await fetch(
+    `${BASE_URL}/?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
+  return await resp.json();
+}
 
 export default {
-    fetchImages
-    
+  fetchImages,
 };
